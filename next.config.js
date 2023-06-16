74082bd57261538e997d0907d7ac7aca987d9fd9
claudiocassimiro <days_surfing@live.com>
2023-06-15 21:51:33 -0300

    fix: hotfix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
+const withPWA = require('next-pwa')({
+  dest: 'public',
   register: true,
   skipWaiting: true,
 });
 
 const nextConfig = {
   images: {
-    domains: ["localhost"],
+    domains: ['localhost'],
   },
   reactStrictMode: true,
   swcMinify: true,
