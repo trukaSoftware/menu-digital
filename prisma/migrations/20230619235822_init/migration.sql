f8053f259779065589f9ac7188145b140593afc7
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-06-19 21:32:13 -0300

    feat: add prisma first migration

---
+++ prisma/migrations/20230619235822_init/migration.sql
@@ -0,0 +1,94 @@
+-- CreateEnum
+CREATE TYPE "CompanyStatus" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- CreateTable
+CREATE TABLE "Company" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "slug" VARCHAR(255) NOT NULL,
+    "status" "CompanyStatus" NOT NULL DEFAULT 'ACTIVE',
+
+    CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Branch" (
+    "id" TEXT NOT NULL,
+    "slug" VARCHAR(255) NOT NULL,
+    "status" "CompanyStatus" NOT NULL DEFAULT 'ACTIVE',
+    "comopanyId" TEXT,
+
+    CONSTRAINT "Branch_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Info" (
+    "id" TEXT NOT NULL,
+    "cnpj" VARCHAR(14) NOT NULL,
+    "email" VARCHAR(45) NOT NULL,
+    "companyLogoUrl" TEXT NOT NULL,
+    "companyThemeUrl" TEXT NOT NULL,
+    "comopanyId" TEXT NOT NULL,
+
+    CONSTRAINT "Info_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" TEXT NOT NULL,
+    "address" VARCHAR(255) NOT NULL,
+    "zipCode" VARCHAR(10) NOT NULL,
+    "infoId" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Products" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "price" MONEY NOT NULL,
+    "comopanyId" TEXT,
+
+    CONSTRAINT "Products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductImages" (
+    "id" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "alt" VARCHAR(500) NOT NULL,
+    "productId" TEXT NOT NULL,
+
+    CONSTRAINT "ProductImages_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Materials" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "grammage" DOUBLE PRECISION,
+    "lastPrice" MONEY,
+    "batchValidity" DATE,
+    "productId" TEXT NOT NULL,
+
+    CONSTRAINT "Materials_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Branch" ADD CONSTRAINT "Branch_comopanyId_fkey" FOREIGN KEY ("comopanyId") REFERENCES "Company"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Info" ADD CONSTRAINT "Info_comopanyId_fkey" FOREIGN KEY ("comopanyId") REFERENCES "Company"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_infoId_fkey" FOREIGN KEY ("infoId") REFERENCES "Info"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Products" ADD CONSTRAINT "Products_comopanyId_fkey" FOREIGN KEY ("comopanyId") REFERENCES "Company"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductImages" ADD CONSTRAINT "ProductImages_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Materials" ADD CONSTRAINT "Materials_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
