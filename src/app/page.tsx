f462de55fe414cfc87dfaa2b0e84c0a5a40f883d
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-06-21 21:10:03 -0300

    style: remove padding on main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex bg-white min-h-screen flex-col items-center justify-between p-1" />
+      <main className="flex bg-white min-h-screen flex-col items-center justify-between" />
     </>
   );
 }
