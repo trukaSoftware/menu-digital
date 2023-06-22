e6881fef710238908a18794b2c0c81ab4b807dce
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-06-21 21:09:26 -0300

    feat: create component Badges

--- src/app/components/Badges/index.tsx
+++ src/app/components/Badges/index.tsx
@@ -1,9 +1,28 @@
+'use client';
+
+import { useState } from 'react';
+
+import Badge from './Badge';
 import styles from './styles.module.css';
 
-export default function Badges() {
+export interface BadgesProps {
+  badges: string[];
+}
+
+export default function Badges({ badges }: BadgesProps) {
+  const [selectedBadge, setSelectedBadge] = useState(``);
+
   return (
-    <div>
-      <p>Ostoporosi</p>
-    </div>
+    <nav className={styles.badges}>
+      {badges.map((badge, index) => (
+        <Badge
+          key={`${++index}-${badge}-${Math.random() * 10000}`}
+          badgeName={badge}
+          linkTo="#"
+          onClick={() => setSelectedBadge(badge)}
+          isSelected={selectedBadge === badge}
+        />
+      ))}
+    </nav>
   );
 }
