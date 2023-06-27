adea40db097e08b3663ef2eb1a24f2515ce4ef1e
claudiocassimiro <days_surfing@live.com>
2023-06-27 11:17:00 -0300

    feat: implements provisory Products page and styles

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header';
+import styles from './styles.module.css';
+
+export default function Products() {
+  return (
+    <>
+      <Header />
+      <main className={styles.mainContainer} />
+    </>
+  );
+}
