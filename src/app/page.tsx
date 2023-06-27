ab713aa22e2880f5ca83b04920e754e9d79b1035
claudiocassimiro <days_surfing@live.com>
2023-06-27 11:16:33 -0300

    refactor: remove Header and main of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import Header from './components/Header';
-
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <main className="flex bg-white min-h-screen flex-col items-center justify-between" />
-    </>
-  );
+  return <></>;
 }
