b6c82783224761c6ec995eded0ea203e5c22860d
claudiocassimiro <days_surfing@live.com>
2023-06-27 14:13:10 -0300

    feat: add new pattern to keep handle class more easily

--- src/app/configs/page.tsx
+++ src/app/configs/page.tsx
@@ -4,8 +4,8 @@ import styles from './styles.module.css';
 
 export default function Configs() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Configs</h1>
+    <div className={styles.configsContainer}>
+      <h1 className={styles.configsTitle}>Configs</h1>
       <UserButton afterSignOutUrl="/sign-in" />
     </div>
   );
