c99cb92f18833fcc0cba27ed9a2a2e49afaddf2d
claudiocassimiro <days_surfing@live.com>
2023-06-27 12:17:41 -0300

    fix: remove unnecessary lines

--- src/app/configs/page.tsx
+++ src/app/configs/page.tsx
@@ -1,19 +1,8 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-
-import { UserButton, useAuth } from '@clerk/nextjs';
+import { UserButton } from '@clerk/nextjs';
 
 import styles from './styles.module.css';
 
 export default function Configs() {
-  const router = useRouter();
-  const { userId } = useAuth();
-
-  if (!userId) {
-    router.push(`/sign-in`);
-  }
-
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Configs</h1>
