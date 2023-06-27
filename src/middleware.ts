d3a165da95fa4a1f202a64422fc44acc6a248338
claudiocassimiro <days_surfing@live.com>
2023-06-27 14:11:45 -0300

    fix: hotfix

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export default authMiddleware({
       return NextResponse.redirect(`${req.nextUrl.href}sign-in`);
     }
 
-    // TODO: The configs page should be unique for each session, so we have to pass the sessionId or tenateId to do this.
+    // TODO: The configs page should be unique for each session, so we have to pass the sessionId or tenantId to do this.
 
     if (req.nextUrl.pathname === `/` && req.cookies.get(`__session`)) {
       return NextResponse.redirect(`${req.nextUrl.href}configs`); // ${req.nextUrl.href}tenantId/configs
