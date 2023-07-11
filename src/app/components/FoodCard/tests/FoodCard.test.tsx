a7a0adbe254f4fd08f172951d00224d41ace7eaa
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-07-11 13:06:58 -0300

    test: create FoodCard component tests

---
+++ src/app/components/FoodCard/tests/FoodCard.test.tsx
@@ -0,0 +1,93 @@
+import { vi } from 'vitest';
+
+import { render, screen, cleanup } from '@testing-library/react';
+
+import FoodCard, { FoodCardProps } from '..';
+
+export const mockFoodCard = {
+  title: `Nome do Produto`,
+  description: `Descrição do produto`,
+  foodImage: `/images/food-image.svg`,
+  price: 10,
+  discountedPrice: 8,
+  discountPercentage: 20,
+} as FoodCardProps;
+
+export const mockFoodCardWithoutDiscount = {
+  title: `Nome do Produto`,
+  description: `Descrição do produto`,
+  foodImage: `/images/food-image.svg`,
+  price: 10,
+} as FoodCardProps;
+
+describe(`FoodCard`, () => {
+  afterAll(() => {
+    vi.clearAllMocks();
+  });
+
+  afterEach(() => {
+    cleanup();
+    vi.resetAllMocks();
+  });
+
+  it(`check if h3 is beeing rendered`, () => {
+    render(<FoodCard {...mockFoodCard} />);
+
+    const heading = screen.getByRole(`heading`, {
+      name: `Nome do Produto`,
+      level: 3,
+    });
+
+    expect(heading).toBeVisible();
+  });
+
+  it(`when prop discountedPrice is not passed, shouldn't render discounted price`, () => {
+    render(<FoodCard {...mockFoodCardWithoutDiscount} />);
+
+    const discountedPrice = screen.queryByText(`R$ 8`);
+
+    expect(discountedPrice).not.toBeInTheDocument();
+  });
+
+  it(`when prop discountedPrice is passed, should render discounted price`, () => {
+    render(<FoodCard {...mockFoodCard} />);
+
+    const discountedPrice = screen.getByText(`R$ 8`);
+
+    expect(discountedPrice).toBeInTheDocument();
+  });
+
+  it(`when prop discountedPrice is not passed, original price shouldn't have style foodCardPriceScratched`, () => {
+    render(<FoodCard {...mockFoodCardWithoutDiscount} />);
+
+    const originalPrice = screen.queryByText(`R$ 10`);
+
+    expect(originalPrice).toBeInTheDocument();
+    expect(originalPrice).not.toHaveClass(`foodCardPriceScratched`);
+  });
+
+  it(`when prop discountedPrice is passed, original price should have style foodCardPriceScratched`, () => {
+    render(<FoodCard {...mockFoodCard} />);
+
+    const originalPrice = screen.getByText(`R$ 10`);
+
+    expect(originalPrice).toBeInTheDocument();
+    expect(originalPrice).toHaveClass(`foodCardPriceScratched`);
+  });
+
+  it(`when prop discountPercentage is not passed, shouldn't render discount percentage`, () => {
+    render(<FoodCard {...mockFoodCardWithoutDiscount} />);
+
+    const discountPercentage = screen.queryByText(`%20`);
+
+    expect(discountPercentage).not.toBeInTheDocument();
+  });
+
+  it(`when prop discountPercentage is passed, should render discount percentage`, () => {
+    render(<FoodCard {...mockFoodCard} />);
+
+    const discountPercentage = screen.getByText(`20%`);
+
+    expect(discountPercentage).toBeInTheDocument();
+  });
+});
