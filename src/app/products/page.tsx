1566c737ba24086ca67487e8923d1355dee048e3
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-07-10 22:57:38 -0300

    feat: add food card to products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,31 @@
+import FoodCard, { FoodCardProps } from '../components/FoodCard';
 import Header from '../components/Header';
 import styles from './styles.module.css';
+
+const mockFoodCard = {
+  title: `Nome do Produto`,
+  description: `Descrição do produto`,
+  foodImage: `/images/food-image.svg`,
+  price: 10,
+  discountedPrice: 8,
+  discountPercentage: 20,
+} as FoodCardProps;
+
+const mockFoodCard2 = {
+  title: `Nome do Produto`,
+  description: `Descrição do produto`,
+  foodImage: `/images/food-image.svg`,
+  price: 10,
+} as FoodCardProps;
 
 export default function Products() {
   return (
     <>
       <Header />
-      <main className={styles.mainContainer} />
+      <main className={styles.mainContainer}>
+        <FoodCard {...mockFoodCard} />
+        <FoodCard {...mockFoodCard2} />
+      </main>
     </>
   );
 }
