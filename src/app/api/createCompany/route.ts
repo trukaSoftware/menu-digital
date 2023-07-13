54ea1de2ed1b6b227a0c48cecb0770834331f0a4
claudiocassimiro <days_surfing@live.com>
2023-07-13 09:15:14 -0300

    feat: implements controller CreateCompany

--- src/app/api/createCompany/route.ts
+++ src/app/api/createCompany/route.ts
@@ -4,30 +4,44 @@ import {
   validateCompanyData,
   CompanyData,
 } from '../../utils/validations/companyDataValidation';
+import { createCompanyService } from '../services/createCompanyService';
 
 export async function POST(req: Request) {
   const {
     id,
     name,
     email,
+    cnpj,
     address,
     zipCode,
     phoneNumber,
     deliveryPhoneNumber,
-  } = req.body as unknown as CompanyData;
+  } = (await req.json()) as CompanyData;
 
   try {
     await validateCompanyData({
       id,
       name,
       email,
+      cnpj,
       address,
       zipCode,
       phoneNumber,
       deliveryPhoneNumber,
     });
 
-    return NextResponse.json({ success: true });
+    const companyId = await createCompanyService({
+      id,
+      name,
+      email,
+      cnpj,
+      address,
+      zipCode,
+      phoneNumber,
+      deliveryPhoneNumber,
+    });
+
+    return NextResponse.json({ companyId });
   } catch (error) {
     return NextResponse.json({ success: false });
   }
