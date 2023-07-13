4877d4d426b618419cc2b04a15d5259ab3048f6e
Eberton <ebertonsouza@unigranrio.br>
2023-07-12 22:27:05 -0300

    feat: create component FoodCardList

---
+++ src/app/components/FoodCardList/index.tsx
@@ -0,0 +1,18 @@
+import FoodCard, { FoodCardProps } from '../FoodCard';
+import styles from './styles.module.css';
+
+export interface FoodCardListProps {
+  title: string;
+  foodCards: FoodCardProps[];
+}
+
+export default function FoodCardList({ title, foodCards }: FoodCardListProps) {
+  return (
+    <section className={styles.foodCardList}>
+      <h2 className={styles.foodCardListTitle}>{title}</h2>
+      {foodCards.map((food) => (
+        <FoodCard {...food} key={food.title} />
+      ))}
+    </section>
+  );
+}
