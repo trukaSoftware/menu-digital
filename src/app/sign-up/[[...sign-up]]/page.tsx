be8659776f8be562022504a0209c77f8a230d8dc
claudiocassimiro <days_surfing@live.com>
2023-07-12 22:43:15 -0300

    feat: add redirectUrl

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 export default function Page() {
   return (
     <div className={styles.containerSignUp}>
-      <SignUp />
+      <SignUp redirectUrl="/createCompany" />
     </div>
   );
 }
