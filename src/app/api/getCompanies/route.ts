3730bf2910e9a43907b8d9696925440e2359220d
claudiocassimiro <days_surfing@live.com>
2023-07-13 09:40:11 -0300

    feat: implements controler getCompanies

---
+++ src/app/api/getCompanies/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+
+import { getCompaniesService } from '../services/getCompaniesService';
+
+export async function GET() {
+  try {
+    const companies = await getCompaniesService();
+
+    return NextResponse.json({ companies });
+  } catch (error) {
+    return NextResponse.json({ success: false });
+  }
+}
