972118995c6a9f958255ad6d371b5a33882c3162
Eberton <ebertonsouza@unigranrio.br>
2023-07-14 17:28:41 -0300

    refactor: add div to foodCard in foodCardList

--- src/app/components/FoodCardList/index.tsx
+++ src/app/components/FoodCardList/index.tsx
@@ -10,9 +10,11 @@ export default function FoodCardList({ title, foodCards }: FoodCardListProps) {
   return (
     <section className={styles.foodCardList}>
       <h2 className={styles.foodCardListTitle}>{title}</h2>
-      {foodCards?.map((food) => (
-        <FoodCard {...food} key={food.title} />
-      ))}
+      <div className={styles.foodCardListFoods}>
+        {foodCards?.map((food) => (
+          <FoodCard {...food} key={food.title} />
+        ))}
+      </div>
     </section>
   );
 }
