de905d912c48a54a219a772d07826e9a3e7a76b0
claudiocassimiro <days_surfing@live.com>
2023-07-17 10:00:42 -0300

    fix: hotfix

--- src/app/api/complements/deleteComplement/route.ts
+++ src/app/api/complements/deleteComplement/route.ts
@@ -6,10 +6,10 @@ export async function DELETE(request: Request) {
   const { searchParams } = new URL(request.url);
   const id = searchParams.get(`id`);
 
-  if (!id)
-    throw new Error(`Por favor, adicione o id do complemento a ser deletado`);
-
   try {
+    if (!id)
+      throw new Error(`Por favor, adicione o id do complemento a ser deletado`);
+
     await deleteComplementService(id);
 
     return NextResponse.json({ deleted: true });
