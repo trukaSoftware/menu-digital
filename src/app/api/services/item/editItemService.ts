4b21a5de591220ee45aee481162e33fb033be7e4
claudiocassimiro <days_surfing@live.com>
2023-07-17 15:29:32 -0300

    feat: implements edit item service

---
+++ src/app/api/services/item/editItemService.ts
@@ -0,0 +1,33 @@
+import { EditItemData } from '@/app/utils/types';
+import prisma from '@/lib/prisma';
+import { Prisma } from '@prisma/client';
+
+export const editItemService = async ({ id, name, price }: EditItemData) => {
+  try {
+    const existingRegister = await prisma.items.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!existingRegister) {
+      throw new Error(`Registro não encontrado para o ID: ${id}`);
+    }
+
+    const updatedData = await prisma.items.update({
+      where: { id },
+      data: {
+        name: name || existingRegister.name,
+        price: price || existingRegister.price,
+      },
+    });
+
+    return updatedData;
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      throw new Error(error.message);
+    }
+
+    return false;
+  }
+};
