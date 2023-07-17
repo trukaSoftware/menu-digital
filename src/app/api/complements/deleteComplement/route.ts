22f8ff53423962b027588303b2fa260d51dcb07d
claudiocassimiro <days_surfing@live.com>
2023-07-17 09:26:09 -0300

    feat: implements delete complement controller

---
+++ src/app/api/complements/deleteComplement/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+
+import { deleteComplementService } from '../../services/complement/deleteComplementService';
+
+export async function DELETE(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get(`id`);
+
+  if (!id)
+    throw new Error(`Por favor, adicione o id do complemento a ser deletado`);
+
+  try {
+    await deleteComplementService(id);
+
+    return NextResponse.json({ deleted: true });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message });
+    }
+
+    throw error;
+  }
+}
