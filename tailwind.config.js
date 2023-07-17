3ca1e8f9d53abccdf6a2075b5468a24e0b47cc6f
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-07-17 17:37:23 -0300

    style: add new width on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      width: {
+        128: '32rem',
+      },
     },
   },
   plugins: [],
