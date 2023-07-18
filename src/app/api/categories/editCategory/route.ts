e65c39f8798c95fd2e59cf4bc13f145538ef7862
claudiocassimiro <days_surfing@live.com>
2023-07-18 10:55:16 -0300

    feat: implements edit category controller

---
+++ src/app/api/categories/editCategory/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+
+import { EditCategoryData } from '@/app/utils/types';
+
+import { editCategoryService } from '../../services/category/editCategoryService';
+
+export async function PUT(req: Request) {
+  const { id, name } = (await req.json()) as EditCategoryData;
+
+  try {
+    if (!id)
+      throw new Error(`Por favor, informe o id do item para fazer alterações`);
+
+    if (!name) throw new Error(`Por favor, informe o novo nome da categoria`);
+
+    const categoryData = await editCategoryService({
+      id,
+      name,
+    });
+
+    return NextResponse.json({ categoryData });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message });
+    }
+  }
+}
