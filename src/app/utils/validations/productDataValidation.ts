6593bd714c3212e77a0d0d2909db40004bb41779
claudiocassimiro <days_surfing@live.com>
2023-07-19 12:37:19 -0300

    feat: implements validation and type to product

---
+++ src/app/utils/validations/productDataValidation.ts
@@ -0,0 +1,25 @@
+import { object, string, array, number, InferType } from 'yup';
+
+const productSchema = object({
+  name: string().required(),
+  description: string().required(),
+  price: number().min(1).required(),
+  categoryId: string().required(),
+  companyId: string().required(),
+  complementId: string(),
+  discount: number(),
+  images: array(
+    object({
+      file: string().required(),
+      name: string().required(),
+      alt: string().required(),
+      width: number(),
+      height: number(),
+    })
+  ),
+});
+
+export type ProductData = InferType<typeof productSchema>;
+
+export const validateProductData = (productData: ProductData) =>
+  productSchema.validate(productData);
