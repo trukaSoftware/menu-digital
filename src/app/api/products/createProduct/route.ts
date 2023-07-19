5aad30e88e3b6c2032f9f5d51a5f78c6bc8fa9df
claudiocassimiro <days_surfing@live.com>
2023-07-19 12:34:43 -0300

    feat: implements create product controller

---
+++ src/app/api/products/createProduct/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from 'next/server';
+
+import {
+  validateProductData,
+  ProductData,
+} from '../../../utils/validations/productDataValidation';
+import { createProductService } from '../../services/product/createProductService';
+
+export async function POST(req: Request) {
+  const {
+    name,
+    description,
+    price,
+    categoryId,
+    companyId,
+    complementId,
+    discount,
+    images,
+  } = (await req.json()) as ProductData;
+
+  try {
+    await validateProductData({
+      name,
+      description,
+      price,
+      categoryId,
+      companyId,
+      complementId,
+      discount,
+      images,
+    });
+
+    const product = await createProductService({
+      name,
+      description,
+      price,
+      categoryId,
+      companyId,
+      complementId,
+      discount,
+      images,
+    });
+
+    return NextResponse.json({ product });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message });
+    }
+  }
+}
