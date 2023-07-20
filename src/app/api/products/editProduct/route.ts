24183d922e5f67f876e2a0550f65c13c2d874d38
claudiocassimiro <days_surfing@live.com>
2023-07-20 10:44:43 -0300

    feat: implements edit product controller

---
+++ src/app/api/products/editProduct/route.ts
@@ -0,0 +1,52 @@
+import { NextResponse } from 'next/server';
+
+import { EditeProductData } from '@/app/utils/types';
+
+import { editProductService } from '../../services/product/editProductService';
+
+export async function PUT(req: Request) {
+  const {
+    id,
+    name,
+    price,
+    description,
+    categoryId,
+    complementsId,
+    complementsToRemove,
+  } = (await req.json()) as EditeProductData;
+
+  try {
+    if (!id)
+      throw new Error(
+        `Por favor, informe o id do produto para fazer alterações`
+      );
+
+    if (
+      !name &&
+      !price &&
+      !description &&
+      !categoryId &&
+      !complementsId &&
+      !complementsToRemove
+    )
+      throw new Error(
+        `Por favor, informe ao menos um campo do produto a ser alterado`
+      );
+
+    const productData = await editProductService({
+      id,
+      name,
+      price,
+      description,
+      categoryId,
+      complementsId,
+      complementsToRemove,
+    });
+
+    return NextResponse.json({ productData });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message });
+    }
+  }
+}
