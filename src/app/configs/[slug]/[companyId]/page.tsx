ba9d1bbeb74ba7836fc20998c2347b5c4ea0287f
claudiocassimiro <days_surfing@live.com>
2023-07-26 13:21:32 -0300

    refactor: use feature of next to fetch api data with axios

--- src/app/configs/[slug]/[companyId]/page.tsx
+++ src/app/configs/[slug]/[companyId]/page.tsx
@@ -1,16 +1,21 @@
-'use client';
-
-import { RouterParams } from '@/app/utils/types';
-import { UserButton } from '@clerk/nextjs';
+import ManageScreenHeader from '@/app/components/ManageScreenHeader';
+import api from '@/app/utils/api';
+import { CompanyProps, RouterParams } from '@/app/utils/types';
 
 import styles from './styles.module.css';
 
-export default function Configs({ params }: RouterParams) {
-  // params can be used to handle company tenant informations
+export default async function Configs({ params }: RouterParams) {
+  const result = await api.get<CompanyProps>(
+    `/company/getCompanyById?id=${params.companyId}`
+  );
+
+  const { name, info } = result.data.companies;
   return (
     <div className={styles.configsContainer}>
-      <h1 className={styles.configsTitle}>Configs</h1>
-      <UserButton afterSignOutUrl="/sign-in" />
+      <ManageScreenHeader
+        companyLogoUrl={info.companyLogoUrl}
+        companyName={name}
+      />
     </div>
   );
 }
