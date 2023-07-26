020703535bc305e675f523629a9d984800b8b286
claudiocassimiro <days_surfing@live.com>
2023-07-26 12:26:48 -0300

    feat: implement ManageScreenHeader component

---
+++ src/app/components/ManageScreenHeader/index.tsx
@@ -0,0 +1,61 @@
+import { SlArrowLeft } from 'react-icons/sl';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { UserButton } from '@clerk/nextjs';
+
+import styles from './styles.module.css';
+
+export type ManageScreenHeaderProps = {
+  companyLogoUrl: string;
+  companyName: string;
+  title?: string;
+  hasBackButton?: boolean;
+  backPage?: string;
+};
+
+export default function ManageScreenHeader({
+  companyLogoUrl,
+  companyName,
+  title,
+  hasBackButton = false,
+  backPage,
+}: ManageScreenHeaderProps) {
+  return (
+    <header
+      data-testid="manage-screen-header"
+      className={
+        hasBackButton
+          ? styles.manageScreenHeaderWithRowReverse
+          : styles.manageScreenHeader
+      }
+    >
+      <Image
+        className={styles.manageScreenHeaderLogo}
+        src={companyLogoUrl}
+        alt={`Imagem do estabelecimento ${companyName}`}
+        width={44}
+        height={44}
+      />
+
+      {title ? (
+        <h1 className={styles.manageScreenHeaderTitle}>{title}</h1>
+      ) : (
+        <h1 className={styles.manageScreenHeaderTitle}>{companyName}</h1>
+      )}
+
+      {hasBackButton ? (
+        <Link
+          role="link"
+          title="Voltar para página anterior"
+          href={backPage || ``}
+        >
+          <SlArrowLeft size="32" color="DC2626" />
+        </Link>
+      ) : (
+        <UserButton afterSignOutUrl="/sign-in" />
+      )}
+    </header>
+  );
+}
