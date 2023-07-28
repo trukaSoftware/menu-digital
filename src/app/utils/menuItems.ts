52715b8e896012467fa842e319ae47ee6d462a2a
claudiocassimiro <days_surfing@live.com>
2023-07-28 12:16:02 -0300

    feat: implements an array of itens to use on MenuComponent

---
+++ src/app/utils/menuItems.ts
@@ -0,0 +1,30 @@
+import { FaGripLines, FaShoppingBasket } from 'react-icons/fa';
+
+export const menuItens = [
+  {
+    menuTitle: `Criar novo(a)`,
+    items: [
+      {
+        menuIcon: FaShoppingBasket,
+        itemText: `Produtos`,
+      },
+      {
+        menuIcon: FaGripLines,
+        itemText: `Categorias`,
+      },
+    ],
+  },
+  {
+    menuTitle: `Gerenciar`,
+    items: [
+      {
+        menuIcon: FaShoppingBasket,
+        itemText: `Produtos`,
+      },
+      {
+        menuIcon: FaGripLines,
+        itemText: `Categorias`,
+      },
+    ],
+  },
+];
