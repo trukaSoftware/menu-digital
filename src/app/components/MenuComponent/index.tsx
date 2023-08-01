693d665183f06570d62b559144fcdc393b2fdd90
claudiocassimiro <days_surfing@live.com>
2023-08-01 10:15:08 -0300

    refactor: apply some change to handle differents behavios with buttons

--- src/app/components/MenuComponent/index.tsx
+++ src/app/components/MenuComponent/index.tsx
@@ -1,21 +1,30 @@
 import { IconType } from 'react-icons';
 
+import Link from 'next/link';
+
 import DefaultDialog from '../DefaultDialog';
 import styles from './styles.module.css';
 
-interface MenuComponentProps {
+export interface MenuComponentProps {
   menuInformations: {
     menuTitle: string;
     items: {
       menuIcon: IconType;
+      dialogHeadLineText?: string;
       itemText: string;
-      DialogBody: React.FunctionComponent;
+      DialogBody?: React.FunctionComponent;
+      managementPageHref?: string;
     }[];
   }[];
+  companyInfos: {
+    slug: string;
+    companyId: string;
+  };
 }
 
 export default function MenuComponent({
   menuInformations,
+  companyInfos,
 }: MenuComponentProps) {
   return (
     <nav className={styles.menuComponentContainer}>
@@ -28,25 +37,43 @@ export default function MenuComponent({
             {menuInformation.menuTitle}
           </h2>
           <div className={styles.menuComponentContainerItems}>
-            {menuInformation.items.map((item) => (
-              <DefaultDialog key={item.itemText}>
-                {[
-                  <button
-                    className={styles.menuComponentItem}
-                    type="button"
-                    key={`button-${item.itemText}`}
-                  >
-                    <div className={styles.menuComponentIconContainer}>
-                      <item.menuIcon size={32} color="E5E7EB" />
-                    </div>
-                    <span className={styles.menuComponentItemText}>
-                      {item.itemText}
-                    </span>
-                  </button>,
-                  <item.DialogBody key={`dialogBody-${item.itemText}`} />,
-                ]}
-              </DefaultDialog>
-            ))}
+            {menuInformation.items.map((item) =>
+              item.DialogBody ? (
+                <DefaultDialog
+                  key={item.itemText}
+                  dialogHeadLineText={item.dialogHeadLineText || ``}
+                >
+                  {[
+                    <button
+                      className={styles.menuComponentItem}
+                      type="button"
+                      key={`button-${item.itemText}`}
+                    >
+                      <div className={styles.menuComponentIconContainer}>
+                        <item.menuIcon size={32} color="E5E7EB" />
+                      </div>
+                      <span className={styles.menuComponentItemText}>
+                        {item.itemText}
+                      </span>
+                    </button>,
+                    <item.DialogBody key={`dialogBody-${item.itemText}`} />,
+                  ]}
+                </DefaultDialog>
+              ) : (
+                <Link
+                  className={styles.menuComponentItem}
+                  href={`${item.managementPageHref}/${companyInfos.slug}/${companyInfos.companyId}`}
+                  key={`button-${item.itemText}`}
+                >
+                  <div className={styles.menuComponentIconContainer}>
+                    <item.menuIcon size={32} color="E5E7EB" />
+                  </div>
+                  <span className={styles.menuComponentItemText}>
+                    {item.itemText}
+                  </span>
+                </Link>
+              )
+            )}
           </div>
         </div>
       ))}
