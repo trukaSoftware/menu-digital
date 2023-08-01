bbf61b10c3a1e4fe2a6bba52439ee64689e3df49
claudiocassimiro <days_surfing@live.com>
2023-08-01 10:14:28 -0300

    refactor: h3 now is a dynamic title

--- src/app/components/DefaultDialog/index.tsx
+++ src/app/components/DefaultDialog/index.tsx
@@ -8,14 +8,17 @@ import styles from './styles.module.css';
 
 interface DefaultDialogProps {
   children: JSX.Element[];
-  key: string;
+  dialogHeadLineText: string;
 }
 
-export default function DefaultDialog({ children, key }: DefaultDialogProps) {
+export default function DefaultDialog({
+  children,
+  dialogHeadLineText,
+}: DefaultDialogProps) {
   const [button, dialogBody] = children;
 
   return (
-    <Dialog.Root key={key}>
+    <Dialog.Root>
       <Dialog.Trigger className={styles.defaultDialogTrigger} asChild>
         {button}
       </Dialog.Trigger>
@@ -23,7 +26,7 @@ export default function DefaultDialog({ children, key }: DefaultDialogProps) {
         <Dialog.Overlay className={styles.defaultDialogOverlay} />
         <Dialog.Content className={styles.defaultDialogContent}>
           <header className={styles.defaultDialogHeader}>
-            <h3 className={styles.defaultDialogTitle}>Criação de produto</h3>
+            <h3 className={styles.defaultDialogTitle}>{dialogHeadLineText}</h3>
             <Dialog.Close
               className={styles.defaultDialogClose}
               aria-label="Fechar modal"
