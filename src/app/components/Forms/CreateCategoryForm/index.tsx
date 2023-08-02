6d9b59e07b2e3c7141df2785aeb9bf5f560ecacc
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-08-01 21:29:27 -0300

    feat: partial made Form

--- src/app/components/Forms/CreateCategoryForm/index.tsx
+++ src/app/components/Forms/CreateCategoryForm/index.tsx
@@ -1,20 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { CgSpinner } from 'react-icons/cg';
 import { FaGripLines, FaSearch } from 'react-icons/fa';
 
 import { InferType } from 'yup';
 
+import { createCategory } from '@/app/utils/api/createCategory';
+import { editProduct } from '@/app/utils/api/editProduct';
+import { removeAccent } from '@/app/utils/removeAccent';
 import { useProducts } from '@/hooks/useProducts';
 import { createCategoryFormSchema } from '@/yup/createCategoryFormSchema';
 import { yupResolver } from '@hookform/resolvers/yup';
 
+import ButtonSubmit from '../../ButtonSubmit';
 import DefaultInput from '../../DefaultInput';
 import styles from './styles.module.css';
 
 type CreateCategoryFormData = InferType<typeof createCategoryFormSchema>;
 
-export default function CreateCategoryForm() {
+export interface CreateCategoryFormProsp {
+  companyId: string;
+}
+
+export default function CreateCategoryForm({
+  companyId,
+}: CreateCategoryFormProsp) {
   const formOptions = {
     resolver: yupResolver(createCategoryFormSchema),
   };
@@ -26,16 +35,44 @@ export default function CreateCategoryForm() {
 
   const [isSubmiting, setIsSubmiting] = useState(false);
   const [searchProductName, setSearchProductName] = useState<string>(``);
+  const [requestError, setRequestError] = useState(false);
 
   const { products } = useProducts();
+  const filteredProducts = products.filter((product) =>
+    removeAccent(product.name.toLowerCase()).includes(
+      removeAccent(searchProductName.toLowerCase())
+    )
+  );
 
   const onSubmit = async (data: CreateCategoryFormData) => {
     setIsSubmiting(true);
-    console.log(data);
+    // const createdCategory = await createCategory({
+    //   name: data.categoryName,
+    //   companyId,
+    // });
+
+    const newCategoryId = `957ee80-7ad7-4052-a3f2-2e747a9239`;
+
+    console.log(`ids dos produtos`, data.productsIds);
+    try {
+      if (data.productsIds) {
+        data.productsIds.forEach(async (productId) => {
+          const editedProduct = await editProduct({
+            id: productId as string,
+            categoryId: newCategoryId,
+          });
+
+          if (!editedProduct?.id) {
+            setRequestError(true);
+          }
+        });
+      }
+    } catch {
+      console.log(`caiu aqui`);
+    }
+
     setIsSubmiting(false);
   };
-
-  console.log(products);
 
   return (
     <form
@@ -66,26 +103,31 @@ export default function CreateCategoryForm() {
           />
         </div>
         <div className={styles.searchProductsList}>
-          <label htmlFor="idDoProduto1" className={styles.searchProductItem}>
-            <input
-              type="checkbox"
-              value="idDoProduto1"
-              id="idDoProduto1"
-              className={styles.searchProductItemCheckbox}
-              {...register(`products`)}
-            />
-            <span className={styles.newCheckBox} />
-            <span>Nome do produto</span>
-          </label>
+          {filteredProducts.map((product) => (
+            <label
+              htmlFor={product.id}
+              className={styles.searchProductItem}
+              key={product.id}
+            >
+              <input
+                type="checkbox"
+                value={product.id}
+                id={product.id}
+                className={styles.searchProductItemCheckbox}
+                {...register(`productsIds`)}
+              />
+              <span className={styles.newCheckBox} />
+              <span>{product.name}</span>
+            </label>
+          ))}
         </div>
       </div>
-      <button className={styles.submitButton} type="submit">
-        {isSubmiting ? (
-          <CgSpinner className="text-white animate-spin-fast" size={26} />
-        ) : (
-          `Criar categoria`
-        )}
-      </button>
+      <ButtonSubmit
+        isSubmiting={isSubmiting}
+        text="Criar categoria"
+        submitError={requestError}
+        className={styles.submitButton}
+      />
     </form>
   );
 }
