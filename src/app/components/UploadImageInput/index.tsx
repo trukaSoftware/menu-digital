10e4038b3268a8f6a40203562954ac3b464267c9
Eberton <ebertonsouza@unigranrio.br>
2023-08-02 18:01:00 -0300

    feat: create uploadImageInput component

---
+++ src/app/components/UploadImageInput/index.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react';
+
+import styles from './styles.module.css';
+
+interface UploadImageInputProps {
+  title: string;
+  handleFileChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  iconImage: ReactNode;
+  id: string;
+  style: string;
+  imageName?: string;
+}
+
+export default function uploadImageInput({
+  iconImage,
+  handleFileChange,
+  title,
+  id,
+  style,
+  imageName,
+}: UploadImageInputProps) {
+  return (
+    <>
+      <div className={styles.imageInput}>
+        <div className={styles.inputIcon}>{iconImage}</div>
+        <label htmlFor={id} className={style}>
+          <span>
+            {title}
+            <p className={styles.teste}>{imageName}</p>
+          </span>
+          <input
+            id={id}
+            type="file"
+            onChange={handleFileChange}
+            className={styles.input}
+          />
+        </label>
+      </div>
+    </>
+  );
+}
