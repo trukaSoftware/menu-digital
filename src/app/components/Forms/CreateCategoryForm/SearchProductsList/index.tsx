070aa934df8d2bda5fc08dff31a889fabda4cfc7
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-08-02 15:58:08 -0300

    feat: create empty state for SearchProductsList

--- src/app/components/Forms/CreateCategoryForm/SearchProductsList/index.tsx
+++ src/app/components/Forms/CreateCategoryForm/SearchProductsList/index.tsx
@@ -1,37 +1,56 @@
 import { UseFormRegisterReturn } from 'react-hook-form';
+import { MdOutlineSearchOff } from 'react-icons/md';
 
 import { Product } from '@/types/product';
+
+import Spinner from '@/app/components/Spinner';
 
 import styles from './styles.module.css';
 
 export interface SearchProductsListProps {
   filteredProducts: Product[];
   register: UseFormRegisterReturn;
+  gettingProducts: boolean;
 }
 
 export default function SearchProductsList({
   filteredProducts,
   register,
+  gettingProducts,
 }: SearchProductsListProps) {
   return (
     <div className={styles.searchProductsList}>
-      {filteredProducts.map((product) => (
-        <label
-          htmlFor={product.id}
-          className={styles.searchProductItem}
-          key={product.id}
-        >
-          <input
-            type="checkbox"
-            value={product.id}
-            id={product.id}
-            className={styles.searchProductItemCheckbox}
-            {...register}
-          />
-          <span className={styles.newCheckBox} />
-          <span>{product.name}</span>
-        </label>
-      ))}
+      {gettingProducts ? (
+        <div className={styles.searchingProducts}>
+          <p>Buscando produtos</p>
+          <Spinner size={32} color="#000" />
+        </div>
+      ) : null}
+
+      {!gettingProducts && filteredProducts.length > 0 ? (
+        filteredProducts.map((product) => (
+          <label
+            htmlFor={product.id}
+            className={styles.searchProductItem}
+            key={product.id}
+          >
+            <input
+              type="checkbox"
+              value={product.id}
+              id={product.id}
+              className={styles.searchProductItemCheckbox}
+              {...register}
+            />
+            <span className={styles.newCheckBox} />
+            <span>{product.name}</span>
+          </label>
+        ))
+      ) : (
+        <div className={styles.noProductsFound}>
+          <MdOutlineSearchOff size={28} />
+          <p>Nenhum produto encontrado</p>
+        </div>
+      )}
     </div>
   );
 }
