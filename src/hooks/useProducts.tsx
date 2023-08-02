6c1ce66dafb0d9c0ed4dc3cf0af3060572aab55b
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-08-02 15:55:27 -0300

    feat: useProducts now returns gettingProducts

--- src/hooks/useProducts.tsx
+++ src/hooks/useProducts.tsx
@@ -6,16 +6,18 @@ import { getter } from '@/app/utils/api/getter';
 
 export function useProducts() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [gettingProducts, setGettingProducts] = useState(true);
 
   const getProductsResponse = async () => {
     const data = await getter<ProductResponse>(`/api/products/getProducts`);
 
     setProducts(data.data.products);
+    setGettingProducts(false);
   };
 
   useEffect(() => {
     getProductsResponse();
   }, []);
 
-  return { products };
+  return { products, gettingProducts };
 }
