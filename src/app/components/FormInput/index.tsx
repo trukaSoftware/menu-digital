6224cdc978ac5d28f073123c0f3535e0d2e149e4
Eberton <ebertonsouza@unigranrio.br>
2023-08-02 17:47:17 -0300

    feat: add formInput component

---
+++ src/app/components/FormInput/index.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
+
+import styles from './styles.module.css';
+
+export interface FormInputProps {
+  title: string;
+  iconImage: ReactNode;
+  id: string;
+  type: string;
+  placeHolder: string;
+  errorMessage: string | undefined;
+  register: UseFormRegisterReturn;
+}
+
+export default function FormInputLabel({
+  title,
+  errorMessage,
+  iconImage,
+  id,
+  type,
+  placeHolder,
+  register,
+}: FormInputProps) {
+  return (
+    <label htmlFor={id}>
+      <span className={styles.formInputTitle}>{title}</span>
+      <div className={styles.formInputWrapper}>
+        <div className={styles.formInputIcon}>{iconImage}</div>
+        <input
+          className={placeHolder ? styles.formInputValue : styles.formInput}
+          type={type}
+          id={id}
+          placeholder={placeHolder}
+          {...register}
+        />
+      </div>
+      <p className={styles.formErrorText}>{errorMessage}</p>
+    </label>
+  );
+}
