28d4904caff60f06eefab3a80ae8b0cd98581ea1
claudiocassimiro <days_surfing@live.com>
2023-08-07 10:10:01 -0300

    refactor: remove dinamic rendering of elements

--- src/app/components/MenuComponent/index.tsx
+++ src/app/components/MenuComponent/index.tsx
@@ -1,82 +1,51 @@
-import { IconType } from 'react-icons';
-
 import Link from 'next/link';
 
-import DefaultDialog from '../DefaultDialog';
+import { menuItens } from '@/app/utils/menuItems';
+
+import CreateCategoryTrigger from '../Dialogs/CreateCategoryDialog/CreateCategoryTrigger';
+import CreateProductTrigger from '../Dialogs/CreateProductDialog/CreateProductTrigger';
 import styles from './styles.module.css';
 
 export interface MenuComponentProps {
-  menuInformations: {
-    menuTitle: string;
-    items: {
-      menuIcon: IconType;
-      dialogHeadLineText?: string;
-      itemText: string;
-      DialogBody?: React.FunctionComponent;
-      managementPageHref?: string;
-    }[];
-  }[];
-  companyInfos: {
+  menuTitle: string;
+  companyInfos?: {
     slug: string;
     companyId: string;
   };
 }
 
 export default function MenuComponent({
-  menuInformations,
+  menuTitle,
   companyInfos,
 }: MenuComponentProps) {
   return (
     <nav className={styles.menuComponentContainer}>
-      {menuInformations.map((menuInformation) => (
-        <div
-          className={styles.menuComponentWrapper}
-          key={menuInformation.menuTitle}
-        >
-          <h2 className={styles.menuComponentTitle}>
-            {menuInformation.menuTitle}
-          </h2>
-          <div className={styles.menuComponentContainerItems}>
-            {menuInformation.items.map((item) =>
-              item.DialogBody ? (
-                <DefaultDialog
-                  key={item.itemText}
-                  dialogHeadLineText={item.dialogHeadLineText || ``}
-                >
-                  {[
-                    <button
-                      className={styles.menuComponentItem}
-                      type="button"
-                      key={`button-${item.itemText}`}
-                    >
-                      <div className={styles.menuComponentIconContainer}>
-                        <item.menuIcon size={32} color="E5E7EB" />
-                      </div>
-                      <span className={styles.menuComponentItemText}>
-                        {item.itemText}
-                      </span>
-                    </button>,
-                    <item.DialogBody key={`dialogBody-${item.itemText}`} />,
-                  ]}
-                </DefaultDialog>
-              ) : (
-                <Link
-                  className={styles.menuComponentItem}
-                  href={`${item.managementPageHref}/${companyInfos.slug}/${companyInfos.companyId}`}
-                  key={`button-${item.itemText}`}
-                >
-                  <div className={styles.menuComponentIconContainer}>
-                    <item.menuIcon size={32} color="E5E7EB" />
-                  </div>
-                  <span className={styles.menuComponentItemText}>
-                    {item.itemText}
-                  </span>
-                </Link>
-              )
-            )}
-          </div>
+      <div className={styles.menuComponentWrapper}>
+        <h2 className={styles.menuComponentTitle}>{menuTitle}</h2>
+        <div className={styles.menuComponentContainerItems}>
+          {companyInfos ? (
+            menuItens.map((item) => (
+              <Link
+                className={styles.menuComponentItem}
+                href={`${item.managementPageHref}/${companyInfos.slug}/${companyInfos.companyId}`}
+                key={`item-${Date.now}`}
+              >
+                <div className={styles.menuComponentIconContainer}>
+                  <item.menuIcon size={32} color="E5E7EB" />
+                </div>
+                <span className={styles.menuComponentItemText}>
+                  {item.itemText}
+                </span>
+              </Link>
+            ))
+          ) : (
+            <>
+              <CreateProductTrigger />
+              <CreateCategoryTrigger />
+            </>
+          )}
         </div>
-      ))}
+      </div>
     </nav>
   );
 }
