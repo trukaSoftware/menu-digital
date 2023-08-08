1b03c2f475dc8c38319197bf8b9c765f7d2699f4
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-08-08 19:01:43 -0300

    feat: add EditableFoodCard to main

--- src/app/configs/[slug]/[companyId]/page.tsx
+++ src/app/configs/[slug]/[companyId]/page.tsx
@@ -1,6 +1,8 @@
+import EditableFoodCard from '@/app/components/EditableFoodCard';
 import ManagementScreenHeader from '@/app/components/ManagementScreenHeader';
 import MenuComponent from '@/app/components/MenuComponent';
 import Teste from '@/app/components/Teste';
+import { foodCardMock } from '@/app/mocks/foodCard';
 import api from '@/app/utils/api';
 import { menuItens } from '@/app/utils/menuItems';
 import { CompanyProps, RouterParams } from '@/app/utils/types';
@@ -22,6 +24,7 @@ export default async function Configs({ params }: RouterParams) {
       <main className={styles.configsMainContainer}>
         <MenuComponent menuInformations={menuItens} />
         <Teste />
+        <EditableFoodCard {...foodCardMock} id="id-teste" />
         <button className={styles.configsSeeMyStoreButton} type="button">
           Como sua loja está hoje
         </button>
