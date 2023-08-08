d6cbb9832514285472037abc6a6711cb4f3b9211
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-08-08 19:12:50 -0300

    refactor: remove EditableFoodCard component from main page

--- src/app/configs/[slug]/[companyId]/page.tsx
+++ src/app/configs/[slug]/[companyId]/page.tsx
@@ -1,8 +1,6 @@
-import EditableFoodCard from '@/app/components/EditableFoodCard';
 import ManagementScreenHeader from '@/app/components/ManagementScreenHeader';
 import MenuComponent from '@/app/components/MenuComponent';
 import Teste from '@/app/components/Teste';
-import { foodCardMock } from '@/app/mocks/foodCard';
 import api from '@/app/utils/api';
 import { menuItens } from '@/app/utils/menuItems';
 import { CompanyProps, RouterParams } from '@/app/utils/types';
@@ -24,7 +22,6 @@ export default async function Configs({ params }: RouterParams) {
       <main className={styles.configsMainContainer}>
         <MenuComponent menuInformations={menuItens} />
         <Teste />
-        <EditableFoodCard {...foodCardMock} id="id-teste" />
         <button className={styles.configsSeeMyStoreButton} type="button">
           Como sua loja está hoje
         </button>
