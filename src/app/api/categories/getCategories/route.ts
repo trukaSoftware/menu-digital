52ccb4bcb4e5093611b9fb641582dba76d7cef07
claudiocassimiro <days_surfing@live.com>
2023-08-11 11:06:37 -0300

    refactor: implements logic to get only companyId categories

--- src/app/api/categories/getCategories/route.ts
+++ src/app/api/categories/getCategories/route.ts
@@ -2,9 +2,17 @@ import { NextResponse } from 'next/server';
 
 import { getCategoriesService } from '../../services/category/getCategoriesService';
 
-export async function GET() {
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const companyId = searchParams.get(`id`);
+
   try {
-    const categories = await getCategoriesService();
+    if (!companyId)
+      throw new Error(
+        `Por favor, adicione o id do estabelecimento a ser buscado`
+      );
+
+    const categories = await getCategoriesService(companyId);
 
     return NextResponse.json({ categories });
   } catch (error) {
