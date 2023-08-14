ff5723f6d6adbeceae1f17fe7cdd4da916cb0b88
Eberton <ebertonsouza@unigranrio.br>
2023-08-14 11:57:49 -0300

    refactor: refact yup validation for createCompany

--- src/app/utils/yup/createCompanyFormValidation.ts
+++ src/app/utils/yup/createCompanyFormValidation.ts
@@ -1,26 +1,30 @@
 import * as yup from 'yup';
 
 export const createCompanyFormValidation = yup.object().shape({
-  name: yup.string().required(`*Esse campo é obrigatório`),
+  name: yup.string().required(`*O Nome do restaurante é obrigatório.`),
   cnpj: yup
     .string()
-    .test(`len`, `*O CNPJ/CPF deve ser válido`, (value?: string) => {
-      if (!value || value.length === 0) {
-        return false;
+    .test(
+      `len`,
+      `*O CNPJ/CPF é obrigatório e deve ser válido.`,
+      (value?: string) => {
+        if (!value || value.length === 0) {
+          return false;
+        }
+        return value.length === 14 || value.length === 11;
       }
-      return value.length === 14 || value.length === 11;
-    })
+    )
     .required(),
   phoneNumber: yup
     .string()
-    .min(10, `*Esse campo é obrigatório`)
-    .max(11, `*O número de telefone deve ser válido`)
+    .min(10, `*O Telefone é obrigatório.`)
+    .max(11, `*O número de telefone deve ser válido.`)
     .required(),
   deliveryPhoneNumber: yup
     .string()
     .test(
       `len`,
-      `*O número de telefone deve ser válido ou estar vázio`,
+      `*O número de telefone deve ser válido ou estar vázio.`,
       (value?: string) => {
         if (!value || value.length === 0) {
           return true;
@@ -30,10 +34,10 @@ export const createCompanyFormValidation = yup.object().shape({
     ),
   zipCode: yup
     .string()
-    .min(8, `*Esse campo é obrigatório`)
-    .max(8, `*Digite um CEP válido`)
+    .min(8, `*O CEP é obrigatório.`)
+    .max(8, `*Digite um CEP válido.`)
     .required(),
-  address: yup.string().required(`*Esse campo é obrigatório`),
+  address: yup.string().required(`*O Endereço é obrigatório.`),
   fileLogo: yup.string(),
   fileCoverCape: yup.string(),
 });
