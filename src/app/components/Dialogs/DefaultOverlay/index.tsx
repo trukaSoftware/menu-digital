707689772e3a7aafef13f2099a18b1d3e9bc26c8
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-08-16 14:33:29 -0300

    fix: delete DefaultOverlay component (was giving error on call)

--- src/app/components/Dialogs/DefaultOverlay/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Overlay } from '@radix-ui/react-dialog';
-
-import styles from './styles.module.css';
-
-export default function DefaultOverlay() {
-  return <Overlay className={styles.defaultOverlay} />;
-}
