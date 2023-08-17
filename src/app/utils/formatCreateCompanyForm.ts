896affeb59226ab9f3fd3f0e5475a1bd961c8af1
Eberton <ebertonsouza@unigranrio.br>
2023-08-17 12:31:08 -0300

    refactor: remove unnecessary function

--- src/app/utils/formatCreateCompanyForm.ts
+++ src/app/utils/formatCreateCompanyForm.ts
@@ -1,21 +1,3 @@
-export const formatPhoneNumber = (phoneNumber: string) => {
-  const ddd = phoneNumber.substring(0, 2);
-  if (phoneNumber.length === 11) {
-    const part1 = phoneNumber.substring(2, 7);
-    const part2 = phoneNumber.substring(7, 11);
-    return `(${ddd}) ${part1}-${part2}`;
-  }
-  const part1 = phoneNumber.substring(2, 6);
-  const part2 = phoneNumber.substring(6, 10);
-  return `(${ddd}) ${part1}-${part2}`;
-};
-
-export const formatZipCode = (zipcode: string) => {
-  const part1 = zipcode.substring(0, 5);
-  const part2 = zipcode.substring(5, 8);
-  return `${part1}-${part2}`;
-};
-
 export const formatCnpj = (cnpj: string) => {
   const part1 = cnpj.substring(0, 2);
   const part2 = cnpj.substring(2, 5);
