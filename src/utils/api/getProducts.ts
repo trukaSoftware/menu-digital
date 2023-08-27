4c2a81ab78d913f06d9f09be1475e88e1752368b
claudiocassimiro <days_surfing@live.com>
2023-08-27 12:05:25 -0300

    refactor: add new query to url

--- src/utils/api/getProducts.ts
+++ src/utils/api/getProducts.ts
@@ -2,8 +2,10 @@ import { Product, ProductResponse } from '@/types/product';
 
 import api from '@/utils/api';
 
-export async function getProducts() {
-  const data = await api.get<ProductResponse>(`/products/getProducts`);
+export async function getProducts(companyId: string) {
+  const data = await api.get<ProductResponse>(
+    `/products/getProducts?id=${companyId}`
+  );
 
   return { products: data?.data?.products as Product[] };
 }
