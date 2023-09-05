c935a4b4e7faf33a035184c2f711d234d225bbec
Eberton <ebertonsouza@unigranrio.br>
2023-09-05 14:08:24 -0300

    feat: add function to Dropdown searchBar

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,34 +1,44 @@
 import { useState } from 'react';
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 
+import { GetCategoryReturn } from '@/types/category';
+
 import EditableCategoryTitle from '../EditableCategoryTitle';
 import SearchInput from '../SearchInput';
 import styles from './styles.module.css';
 
 export interface DropdownProps {
   removeCategoryFromList: (categoryId: string) => void;
+  categories: GetCategoryReturn[];
 }
 
 const mockTeste = [
   {
-    categoryName: `Mais vendidos`,
-    categoryId: `1`,
-    products: [`Kiut`, `Hamburguer`, `Batata frita`],
+    name: `Mais vendidos`,
+    id: `1`,
+    categoryProducts: [`Kiut`, `Hamburguer`, `Batata frita`],
   },
   {
-    categoryName: `Comidas`,
-    categoryId: `2`,
-    products: [`Kiut`, `Hamburguer`, `Batata frita`],
+    name: `Comidas`,
+    id: `2`,
+    categoryProducts: [
+      `Kiut`,
+      `Hamburguer`,
+      `Batata frita`,
+      `Barata corada`,
+      `Batata rostie`,
+    ],
   },
   {
-    categoryName: `Bebidas`,
-    categoryId: `3`,
-    products: [`Kiut`, `Hamburguer`, `Batata frita`],
+    name: `Bebidas`,
+    id: `3`,
+    categoryProducts: [`Kiut`, `Hamburguer`, `Batata frita`],
   },
 ];
 
 export default function Dropdown({ removeCategoryFromList }: DropdownProps) {
   const [showDropdown, setShowDropdown] = useState<number | null>(null);
+  const [search, setSearch] = useState(``);
 
   const toggleDropdown = (index: number) => {
     if (showDropdown === index) {
@@ -37,15 +47,19 @@ export default function Dropdown({ removeCategoryFromList }: DropdownProps) {
     setShowDropdown(index);
   };
 
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setSearch(value.toLocaleLowerCase());
+  };
+
   return (
-    <div className={styles.dropdownWrapper}>
+    <>
       {mockTeste.map((category, index) => (
-        <>
-          <label key={category.categoryId} htmlFor={category.categoryName}>
+        <div key={category.id} className={styles.dropdownWrapper}>
+          <label htmlFor={category.name}>
             <input
               type="checkbox"
-              name=""
-              id={category.categoryName}
+              id={category.name}
               className={styles.dropdownCheckbox}
               onClick={() => toggleDropdown(index)}
             />
@@ -54,12 +68,10 @@ export default function Dropdown({ removeCategoryFromList }: DropdownProps) {
                 <div className={styles.dropdownCategoryImage}>
                   {showDropdown === index ? <FaChevronUp /> : <FaChevronDown />}
                 </div>
-                <h2 className={styles.dropdownCategoryName}>
-                  {category.categoryName}
-                </h2>
+                <h2 className={styles.dropdownCategoryName}>{category.name}</h2>
               </div>
               <EditableCategoryTitle
-                categoryId={category.categoryId}
+                categoryId={category.id}
                 removeCategoryFromList={removeCategoryFromList}
               />
             </div>
@@ -74,16 +86,22 @@ export default function Dropdown({ removeCategoryFromList }: DropdownProps) {
             <SearchInput
               id={String(index)}
               placeholder="Pesquisar produto por nome..."
+              onChange={handleSearch}
             />
-            {category.products.map((product) => (
+            {(search
+              ? category.categoryProducts.filter((product) =>
+                  product.toLocaleLowerCase().includes(search)
+                )
+              : category.categoryProducts
+            ).map((product) => (
               <label htmlFor={product} key={product}>
                 <input type="checkbox" name="" id={product} />
-                <span>{product}</span>
+                <span className={styles.dropdownProductName}>{product}</span>
               </label>
             ))}
           </div>
-        </>
+        </div>
       ))}
-    </div>
+    </>
   );
 }
