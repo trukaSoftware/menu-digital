7fa51f664e8b9766836c5e0f5ce0d5f82bad045d
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-09-20 18:20:03 -0300

    fix: fix tests

--- src/components/Dropdown/tests/Dropdown.test.tsx
+++ src/components/Dropdown/tests/Dropdown.test.tsx
@@ -6,7 +6,6 @@ import { categoryMock } from '@/mocks/categories';
 import { productsMock } from '@/mocks/products';
 import { renderWithRedux } from '@/testsUtils/providers';
 import { screen, cleanup } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 vi.mock(`@clerk/nextjs`, () => ({
   __esModule: true,
@@ -43,7 +42,7 @@ describe(`Dropdown`, () => {
     vi.resetAllMocks();
   });
 
-  it(`When the component is rendered, there should be a category dropdown button`, async () => {
+  it(`When the component is rendered and showDropdown is null, there should be a closed dropdown`, async () => {
     renderWithRedux(<Dropdown {...mockProps} />);
 
     const categoryDropdownButton = screen.getByRole(`heading`, {
@@ -63,25 +62,7 @@ describe(`Dropdown`, () => {
     expect(fourthCategoryProduct).not.toBeInTheDocument();
   });
 
-  it(`When the dropdown is clicked, there must be products inside it`, async () => {
-    renderWithRedux(<Dropdown {...mockProps} />);
-
-    const categoryDropdownButton = screen.getByRole(`heading`, {
-      name: /reuniao/i,
-    });
-
-    let firstCategoryProduct = screen.queryByText(/pão/i);
-    let secondCategoryProduct = screen.queryByText(/guaraná antartica/i);
-    let thirdCategoryProduct = screen.queryByText(/guaraná jesus/i);
-    let fourthCategoryProduct = screen.queryByText(/smash burguer/i);
-
-    expect(firstCategoryProduct).not.toBeInTheDocument();
-    expect(secondCategoryProduct).not.toBeInTheDocument();
-    expect(thirdCategoryProduct).not.toBeInTheDocument();
-    expect(fourthCategoryProduct).not.toBeInTheDocument();
-
-    await userEvent.click(categoryDropdownButton);
-
+  it(`When the component is rendered and showDropdown is not null, there should be a dropdown menu open with products inside it`, async () => {
     const modifiedMockProps = {
       ...mockProps,
       showDropdown: 0,
@@ -89,10 +70,10 @@ describe(`Dropdown`, () => {
 
     renderWithRedux(<Dropdown {...modifiedMockProps} />);
 
-    firstCategoryProduct = await screen.findByText(/pão/i);
-    secondCategoryProduct = await screen.findByText(/guaraná antartica/i);
-    thirdCategoryProduct = await screen.findByText(/guaraná jesus/i);
-    fourthCategoryProduct = await screen.findByText(/smash burguer/i);
+    const firstCategoryProduct = screen.queryByText(/pão/i);
+    const secondCategoryProduct = screen.queryByText(/guaraná antartica/i);
+    const thirdCategoryProduct = screen.queryByText(/guaraná jesus/i);
+    const fourthCategoryProduct = screen.queryByText(/smash burguer/i);
 
     expect(firstCategoryProduct).toBeInTheDocument();
     expect(secondCategoryProduct).toBeInTheDocument();
