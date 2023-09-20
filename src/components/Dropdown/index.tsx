53fc4ec83d1b731bc42a35b75d6bc4d001b221d8
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-09-20 11:03:37 -0300

    refactor: refactor component dropdown to run right logic

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,94 +1,186 @@
-import { useState } from 'react';
-import { UseFormRegisterReturn } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
+
+import { InferType } from 'yup';
 
 import { GetCategoryReturn } from '@/types/category';
 import { Product } from '@/types/product';
 
+import { editManyProductsCategory } from '@/utils/api/editManyProductsCategory';
+import { EditManyProductsCategoryData } from '@/utils/types';
+
+import { setCategories } from '@/redux/features/categories-slice';
+import { createCategoryFormSchema } from '@/yup/front/createCategoryFormSchema';
+import { useUser } from '@clerk/nextjs';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import ButtonSubmit from '../ButtonSubmit';
 import EditableCategoryTitle from '../EditableCategoryTitle';
 import SearchProductsList from '../Forms/CreateCategoryForm/SearchProductsList';
 import SearchInput from '../SearchInput';
 import styles from './styles.module.css';
 
+type EditCategoryFormData = InferType<typeof createCategoryFormSchema>;
+
 export interface DropdownProps {
-  categories: GetCategoryReturn[];
+  category: GetCategoryReturn;
   filteredProducts: Product[];
-  register: UseFormRegisterReturn;
+  currentCategoryIndex: number;
   gettingProducts: boolean;
+  showDropdown: null | number;
+  search: string;
+  setSearch: (value: string) => void;
+  toggleDropdown: (value: number) => void;
 }
 
 export default function Dropdown({
-  categories,
+  category,
   filteredProducts,
+  currentCategoryIndex,
   gettingProducts,
-  register,
+  showDropdown,
+  search,
+  setSearch,
+  toggleDropdown,
 }: DropdownProps) {
-  const [showDropdown, setShowDropdown] = useState<number | null>(null);
-  const [search, setSearch] = useState(``);
+  const [registredWithSucess, setRegistredWithSucess] = useState(false);
+  const [isSubmiting, setIsSubmiting] = useState(false);
+  const [requestError, setRequestError] = useState(false);
+  const dispatch = useDispatch();
+  const { user } = useUser();
 
-  const toggleDropdown = (index: number) => {
-    if (showDropdown === index) {
-      return setShowDropdown(null);
-    }
-    setSearch(``);
-    setShowDropdown(index);
-  };
+  const productsIds = category?.categoryProducts?.map((product) => product.id);
+
+  const { register, handleSubmit, reset } = useForm({
+    resolver: yupResolver(createCategoryFormSchema),
+    mode: `onChange`,
+    defaultValues: {
+      categoryName: category?.name,
+      productsIds,
+    },
+  });
+
+  useEffect(() => {
+    reset({
+      categoryName: category?.name,
+      productsIds,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [category]);
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
     setSearch(value.toLocaleLowerCase());
   };
 
+  const onSubmit = async (data: EditCategoryFormData) => {
+    setIsSubmiting(true);
+    const productsToRemoveId =
+      productsIds?.filter(
+        (productId) => !data.productsIds?.includes(productId)
+      ) || [];
+
+    const productsToAddId =
+      data.productsIds?.filter(
+        (productId) => !productsIds?.includes(productId as string)
+      ) || [];
+
+    try {
+      const shouldEditCategoryName = data.categoryName !== category?.name;
+      const shouldEditProductsCategoryId =
+        productsToRemoveId.length > 0 ||
+        productsToAddId.length > 0 ||
+        shouldEditCategoryName;
+
+      if (shouldEditProductsCategoryId) {
+        const editManyProductsCategoryPayload = {
+          id: category.id,
+          newCategoryName: shouldEditCategoryName ? data.categoryName : ``,
+          productsToAddId,
+          productsToRemoveId,
+        } as EditManyProductsCategoryData;
+
+        const updatedCategories = await editManyProductsCategory(
+          editManyProductsCategoryPayload,
+          user?.id as string
+        );
+
+        dispatch(setCategories(updatedCategories));
+      }
+    } catch {
+      setRequestError(true);
+    } finally {
+      setIsSubmiting(false);
+    }
+
+    setRegistredWithSucess(true);
+    setIsSubmiting(false);
+  };
+
   return (
-    <>
-      {categories.map((category, index) => (
-        <div key={category.id} className={styles.dropdownWrapper}>
-          <label htmlFor={category.name}>
-            <input
-              type="checkbox"
-              id={category.name}
-              className={styles.dropdownCheckbox}
-              onClick={() => toggleDropdown(index)}
-            />
-            <div className={styles.dropdownCategoryImageWrapper}>
-              <div className={styles.dropdownCategoryImage}>
-                {showDropdown === index ? <FaChevronUp /> : <FaChevronDown />}
-              </div>
-              <EditableCategoryTitle
-                categoryName={category.name}
-                categoryId={category.id}
-              />
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.dropdownForm}>
+      <div className={styles.dropdownWrapper}>
+        <label htmlFor={category.name}>
+          <input
+            type="checkbox"
+            id={category.name}
+            className={styles.dropdownCheckbox}
+            onClick={() => toggleDropdown(currentCategoryIndex)}
+          />
+          <div className={styles.dropdownCategoryImageWrapper}>
+            <div className={styles.dropdownCategoryImage}>
+              {showDropdown === currentCategoryIndex ? (
+                <FaChevronUp />
+              ) : (
+                <FaChevronDown />
+              )}
             </div>
-          </label>
-          <div
-            className={
-              showDropdown === index
-                ? styles.dropdownCategoryContent
-                : styles.dropdownCategoryContentHidden
-            }
-          >
-            <SearchInput
-              id={String(index)}
-              placeholder="Pesquisar produto por nome..."
-              onChange={handleSearch}
-              value={search}
+            <EditableCategoryTitle
+              categoryName={category.name}
+              categoryId={category.id}
             />
-            {showDropdown === index && (
-              <SearchProductsList
-                filteredProducts={
-                  search
-                    ? filteredProducts.filter((product) =>
-                        product.name.toLocaleLowerCase().includes(search)
-                      )
-                    : filteredProducts
-                }
-                gettingProducts={gettingProducts}
-                register={register}
-              />
-            )}
           </div>
+        </label>
+        <div
+          className={
+            showDropdown === currentCategoryIndex
+              ? styles.dropdownCategoryContent
+              : styles.dropdownCategoryContentHidden
+          }
+        >
+          <SearchInput
+            id={String(currentCategoryIndex)}
+            placeholder="Pesquisar produto por nome..."
+            onChange={handleSearch}
+            value={search}
+          />
+          {showDropdown === currentCategoryIndex && (
+            <SearchProductsList
+              filteredProducts={
+                search
+                  ? filteredProducts.filter((product) =>
+                      product.name.toLocaleLowerCase().includes(search)
+                    )
+                  : filteredProducts
+              }
+              gettingProducts={gettingProducts}
+              register={register(`productsIds`)}
+            />
+          )}
+          <ButtonSubmit
+            isSubmiting={isSubmiting}
+            text={
+              registredWithSucess
+                ? `Categoria editada com sucesso ✔️`
+                : `Editar categoria`
+            }
+            submitError={requestError}
+            className={styles.dropdownSubmitButton}
+          />
         </div>
-      ))}
-    </>
+      </div>
+    </form>
   );
 }
