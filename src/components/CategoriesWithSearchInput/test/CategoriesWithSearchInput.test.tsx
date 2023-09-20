a62aaae3a90326132ee52cea289649e30b3f0af5
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-09-20 11:02:36 -0300

    feat: implements testos to CategoriesWithSearchInput

---
+++ src/components/CategoriesWithSearchInput/test/CategoriesWithSearchInput.test.tsx
@@ -0,0 +1,171 @@
+import { vi } from 'vitest';
+
+import { categoriesMock, categoryMock } from '@/mocks/categories';
+import { renderWithRedux } from '@/testsUtils/providers';
+import { screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import CategoriesWithSearchInput from '..';
+
+vi.mock(`@clerk/nextjs`, () => ({
+  __esModule: true,
+  useUser: () => ({
+    user: {
+      id: `1`,
+      emailAddresses: [
+        {
+          emailAddress: `test@example.com`,
+        },
+      ],
+    },
+  }),
+}));
+
+describe(`When CategoriesWithSearchInput is called and`, () => {
+  afterAll(() => {
+    vi.clearAllMocks();
+  });
+
+  afterEach(() => {
+    cleanup();
+    vi.resetAllMocks();
+  });
+
+  it(`does not have categories to display, the message "Não existem categorias cadastradas" should be displayed`, async () => {
+    renderWithRedux(<CategoriesWithSearchInput />, {
+      preloadedState: {
+        categoriesReducer: {
+          categories: [],
+        },
+        productsReducer: {
+          products: [],
+        },
+      },
+    });
+
+    const emptyState = await screen.findByText(
+      `Não existem categorias cadastradas`
+    );
+
+    expect(emptyState).toBeInTheDocument();
+  });
+
+  it(`have categories to display, should displayer all categories of categories array as dropdown buttons`, async () => {
+    renderWithRedux(<CategoriesWithSearchInput />);
+
+    const firstCategoryDropdownButton = screen.getByRole(`heading`, {
+      name: /reuniao/i,
+    });
+
+    const secondCategoryDropdownButton = screen.getByRole(`heading`, {
+      name: /comida/i,
+    });
+
+    expect(firstCategoryDropdownButton).toBeInTheDocument();
+    expect(secondCategoryDropdownButton).toBeInTheDocument();
+  });
+
+  it(`have categories to display, the first category dropdown button must have its 2 products marked in checkboxes`, async () => {
+    renderWithRedux(<CategoriesWithSearchInput />, {
+      preloadedState: {
+        categoriesReducer: {
+          categories: [categoryMock],
+        },
+      },
+    });
+
+    const firstCategoryDropdownButton = screen.getByRole(`heading`, {
+      name: /reuniao/i,
+    });
+
+    await userEvent.click(firstCategoryDropdownButton);
+
+    const checkBoxes = await screen.findAllByTestId(
+      `checkbox-810640e3-a1ec-414b-ad14-deb1dd3f2989`
+    );
+
+    expect(checkBoxes).toHaveLength(2);
+    checkBoxes.forEach((checkBox) => {
+      expect(checkBox).toBeChecked();
+    });
+  });
+
+  it(`have categories to display, the second category dropdown button must have its 2 products marked in checkboxes`, async () => {
+    renderWithRedux(<CategoriesWithSearchInput />, {
+      preloadedState: {
+        categoriesReducer: {
+          categories: [categoriesMock[1]],
+        },
+      },
+    });
+
+    const secondCategoryDropdownButton = screen.getByRole(`heading`, {
+      name: /comida/i,
+    });
+
+    await userEvent.click(secondCategoryDropdownButton);
+
+    const checkBoxes = await screen.findAllByTestId(
+      `checkbox-810640e3-a1ec-414b-ad14-deb1dd3f2990`
+    );
+
+    expect(checkBoxes).toHaveLength(2);
+    checkBoxes.forEach((checkBox) => {
+      expect(checkBox).toBeChecked();
+    });
+  });
+
+  it(`inside the categories dropdowns, only shows the product that was searched for`, async () => {
+    renderWithRedux(<CategoriesWithSearchInput />, {
+      preloadedState: {
+        categoriesReducer: {
+          categories: [categoriesMock[1]],
+        },
+      },
+    });
+
+    const firstCategoryDropdownButton = screen.getByRole(`heading`, {
+      name: /comida/i,
+    });
+
+    await userEvent.click(firstCategoryDropdownButton);
+
+    const searchInput = screen.getAllByPlaceholderText(
+      `Pesquisar produto por nome...`
+    )[0];
+
+    expect(searchInput).toBeInTheDocument();
+
+    const firstCategoryProduct = await screen.findByText(/pão/i);
+
+    await userEvent.type(searchInput, `smash burguer`);
+
+    const searchedProduct = await screen.findByText(/smash burguer/i);
+
+    expect(firstCategoryProduct).not.toBeInTheDocument();
+    expect(searchedProduct).toBeInTheDocument();
+  });
+
+  it(`has categories to display, must render 1 dropdown at a time when clicked`, async () => {
+    renderWithRedux(<CategoriesWithSearchInput />);
+
+    const firstCategoryDropdownButton = screen.getByRole(`heading`, {
+      name: /reuniao/i,
+    });
+    const secondCategoryDropdownButton = screen.getByRole(`heading`, {
+      name: /comida/i,
+    });
+
+    await userEvent.click(firstCategoryDropdownButton);
+
+    const firstCategoryProduct = await screen.findAllByText(/pão/i);
+
+    expect(firstCategoryProduct).toHaveLength(2);
+
+    await userEvent.click(secondCategoryDropdownButton);
+
+    const secondCategoryProduct = await screen.findAllByText(/pão/i);
+
+    expect(secondCategoryProduct).toHaveLength(2);
+  });
+});
