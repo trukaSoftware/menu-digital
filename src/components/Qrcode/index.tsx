e80e070e19c7d0cbc49fa3e91621f886f4e9c127
Eberton <ebertonsouza@unigranrio.br>
2023-09-27 00:54:10 -0300

    build: add download button to qrcode

--- src/components/Qrcode/index.tsx
+++ src/components/Qrcode/index.tsx
@@ -5,16 +5,30 @@ import { useUser } from '@clerk/nextjs';
 import styles from './styles.module.css';
 
 export default function Qrcode() {
+  const user = useUser();
+
   const fullURL = window.location.href;
   const urlObj = new URL(fullURL);
   const pathname = urlObj.origin;
 
-  const user = useUser();
   const qrcodeProductsUrl = `${pathname}/produtos/${user.user?.publicMetadata.slug}/${user.user?.id}`;
 
+  const handleSaveQrcode = () => {
+    window.print();
+  };
+
   return (
-    <div className={styles.qrcodeWrapper}>
-      <QRCode value={qrcodeProductsUrl} />
-    </div>
+    <>
+      <div className={styles.qrcodeWrapper}>
+        <QRCode value={qrcodeProductsUrl} />
+        <button
+          type="button"
+          className={styles.qrcodeButton}
+          onClick={handleSaveQrcode}
+        >
+          Salvar QRcode
+        </button>
+      </div>
+    </>
   );
 }
