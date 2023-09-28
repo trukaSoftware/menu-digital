5888c6f3cc1e040d47d7e091611323df05fd5e1a
Eberton <ebertonsouza@unigranrio.br>
2023-09-28 00:32:24 -0300

    refactor: change how to save qrcode img

--- src/components/QrCode/index.tsx
+++ src/components/QrCode/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import QRCode from 'react-qr-code';
 
 import { useUser } from '@clerk/nextjs';
@@ -6,6 +7,7 @@ import styles from './styles.module.css';
 
 export default function QrCode() {
   const { user } = useUser();
+  const [qrCodeButtonTitle, setQrCodeButtonTitle] = useState(`Salvar QRCode`);
 
   const fullURL = window.location.href;
   const urlObj = new URL(fullURL);
@@ -13,20 +15,37 @@ export default function QrCode() {
 
   const qrCodeProductsUrl = `${pathname}/produtos/${user?.publicMetadata.slug}/${user?.id}`;
 
-  const handleSaveQrCode = () => {
-    window.print();
+  const handleSaveQrCode = async () => {
+    const canvas = document.getElementById(
+      `productsQrCode`
+    ) as HTMLCanvasElement;
+    const image = new Image();
+    image.src = `data:image/svg+xml;base64,${btoa(
+      new XMLSerializer().serializeToString(canvas)
+    )}`;
+    try {
+      const downloadLink = document.createElement(`a`);
+      downloadLink.href = image.src;
+      downloadLink.download = `QRCode-${user?.publicMetadata.slug}`;
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+      setQrCodeButtonTitle(`QRCode salvo!`);
+    } catch (error) {
+      setQrCodeButtonTitle(`Erro ao salvar, tente novamente.`);
+    }
   };
 
   return (
     <>
       <div className={styles.qrCodeWrapper}>
-        <QRCode value={qrCodeProductsUrl} />
+        <QRCode value={qrCodeProductsUrl} id="productsQrCode" />
         <button
           type="button"
           className={styles.qrCodeButton}
           onClick={handleSaveQrCode}
         >
-          Salvar QRCode
+          {qrCodeButtonTitle}
         </button>
       </div>
     </>
