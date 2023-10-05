9982622cc151800d85aa2019da95a50840f49b8d
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-10-05 16:51:52 -0300

    test: add tests for ProductsEmptyState

---
+++ src/components/ProductsEmptyState/tests/ProductsEmptyState.test.tsx
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import { vi } from 'vitest';
+
+import { categoriesMock } from '@/mocks/categories';
+import { renderWithRedux } from '@/testsUtils/providers';
+import { screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import ProductsEmptyState from '..';
+
+vi.mock(`@clerk/nextjs`, () => ({
+  __esModule: true,
+  useUser: () => ({
+    user: {
+      id: `1`,
+    },
+  }),
+}));
+
+vi.mock(`axios`);
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe(`ProductsEmptyState`, () => {
+  afterAll(() => {
+    vi.clearAllMocks();
+  });
+
+  afterEach(() => {
+    cleanup();
+    vi.resetAllMocks();
+  });
+
+  it(`should open create category modal when clicking on button "Cadastrar categoria"`, async () => {
+    mockedAxios.get.mockResolvedValueOnce({
+      data: { categories: categoriesMock },
+    });
+
+    renderWithRedux(<ProductsEmptyState />);
+
+    const emptyStateNewProductBtn = screen.getByRole(`button`, {
+      name: `Cadastrar produto`,
+    });
+
+    expect(emptyStateNewProductBtn).toBeInTheDocument();
+
+    await userEvent.click(emptyStateNewProductBtn);
+
+    expect(
+      screen.getByRole(`heading`, { name: `Criação de produto`, level: 2 })
+    ).toBeInTheDocument();
+  });
+});
