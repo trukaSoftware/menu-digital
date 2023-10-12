dab89e69d9d2e04a943fa9b9aa8809cdf1c65faa
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-10-12 19:55:21 -0300

    feat: create complements needed types

---
+++ src/types/complement.ts
@@ -0,0 +1,17 @@
+export interface ComplementItem {
+  id: string;
+  name: string;
+  price: string;
+  complementId: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Complement {
+  id: string;
+  name: string;
+  maxAmount: number;
+  createdAt: string;
+  updatedAt: string;
+  items: ComplementItem[];
+}
