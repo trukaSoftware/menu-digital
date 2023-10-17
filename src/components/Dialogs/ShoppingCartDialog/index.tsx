3f8e3ea4102d7d136af75cc1fe59882bf0dfba90
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-10-17 17:06:57 -0300

    refactor: add logic to get cardData from localStorage and dispatch than

--- src/components/Dialogs/ShoppingCartDialog/index.tsx
+++ src/components/Dialogs/ShoppingCartDialog/index.tsx
@@ -1,8 +1,13 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FaShoppingBag } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
 
+import { CartItemProps } from '@/components/FoodCardDialog';
+
+import { getCartItens, setCartItens } from '@/redux/features/cartItem-slice';
+import { useAppSelector } from '@/redux/store';
 import { Root, Trigger } from '@radix-ui/react-dialog';
 
 import ShoppingCartPortal from './ShoppingCartPortal';
@@ -10,16 +15,29 @@ import styles from './styles.module.css';
 
 export default function ShoppingCartDialog() {
   const [showDialog, setShowDialog] = useState(false);
+  const cartItens = useAppSelector((state) => getCartItens(state));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const cartData =
+      (JSON.parse(
+        `${localStorage.getItem(`md-food-cart-items`)}`
+      ) as CartItemProps[]) || [];
+
+    dispatch(setCartItens([...cartData]));
+  }, [dispatch]);
 
   return (
     <Root open={showDialog} onOpenChange={setShowDialog}>
       <Trigger type="button" className={styles.shoppingCartDialogTrigger}>
         <FaShoppingBag size={24} fill="#fff" />
-        <div className={styles.shoppingCartDialogContainerQuantityOfProducts}>
-          1
-        </div>
+        {cartItens.length > 0 ? (
+          <div className={styles.shoppingCartDialogContainerQuantityOfProducts}>
+            {cartItens.length}
+          </div>
+        ) : null}
       </Trigger>
-      <ShoppingCartPortal setShowDialog={setShowDialog} />
+      <ShoppingCartPortal cartItens={cartItens} setShowDialog={setShowDialog} />
     </Root>
   );
 }
