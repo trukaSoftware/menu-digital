4ec70544430027586c631312e71f9078482b57cc
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-10-19 09:28:59 -0300

    refactor: add improvement following solid priciples

--- src/components/Dialogs/ShoppingCartDialog/ShoppingCartPortal/index.tsx
+++ src/components/Dialogs/ShoppingCartDialog/ShoppingCartPortal/index.tsx
@@ -1,20 +1,20 @@
 import { useEffect, useState } from 'react';
-import { FaRegEdit, FaShoppingBag } from 'react-icons/fa';
-import { FaMinus, FaPlus } from 'react-icons/fa6';
+import { FaShoppingBag } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 
-import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
 import axios from 'axios';
 
 import { CartItemProps } from '@/components/FoodCardDialog';
 import ModalDefaultHeader from '@/components/ModalDefaultHeader';
+import ShoppingCartProducts from '@/components/ShoppingCartProducts';
 
+import { createOrderToSendToWpp } from '@/utils/createOrderToSendToWpp';
 import { priceToBrazilCurrency } from '@/utils/priceToBrazilCurrency';
 
 import { setCartItens } from '@/redux/features/cartItem-slice';
-import { useUser } from '@clerk/nextjs';
 import { Portal, Overlay, Content } from '@radix-ui/react-dialog';
 
 import styles from './styles.module.css';
@@ -36,7 +36,7 @@ export default function ShoppingCartPortal({
   const [generatingOrder, setGeneratingOrder] = useState(false);
   const [totalPrice, setTotalPrice] = useState<number>(0);
   const dispatch = useDispatch();
-  const { user } = useUser();
+  const pathnames = usePathname();
 
   useEffect(() => {
     const total = cartItens.reduce(
@@ -110,7 +110,7 @@ export default function ShoppingCartPortal({
     try {
       const response = await axios.post(`/api/requests/createRequest`, {
         products: JSON.stringify(products),
-        companyId: user?.id,
+        companyId: pathnames.split(`/`)?.[3],
         status: `CLOSE`, // for delivery requests status should be close, for presential requests status should be OPEN
         table: null, // when the qrcode logic was implemented, we get the table number from url
         totalValue: totalPrice,
@@ -118,54 +118,18 @@ export default function ShoppingCartPortal({
 
       const { request } = response.data;
 
-      const order = {
-        orderNumber: request.id.slice(0, 8),
-        menuName: companyData.name,
-        clientName: `Mock nome`,
-        clientPhoneNumber: `Mock client phone number`,
-        paymentMethod: `Mock paymentMethod`,
-        deliveryType: `Mock deliveryType`,
-        deliveryTime: `Entre 35 e 45 minutos`,
-        deliveryAddress: `Rua 19, próximo a oficina de pitoco, tem um renault kwid da frente`,
+      const whatsappURL = createOrderToSendToWpp({
+        id: request.id,
+        companyData,
         products,
-      };
-
-      let resumoPedidoTexto = `\nRESUMO DO PEDIDO:\n`;
-
-      order.products.forEach((product) => {
-        if (product.complements.length > 0) {
-          resumoPedidoTexto += `\n${product.name}\n\nComplementos do Pedido:\n\n`;
-          product.complements.forEach((complemento) => {
-            resumoPedidoTexto += `${complemento}\n`;
-          });
-
-          resumoPedidoTexto += `\n>>>\n`;
-
-          return;
-        }
-        resumoPedidoTexto += `\n${product.name}\n\n>>>\n`;
+        totalPrice,
       });
-
-      const whatsappURL = `https://wa.me/55${
-        companyData.deliveryPhoneNumber
-      }?text=${encodeURIComponent(
-        `Número do Pedido: ${order.orderNumber}\n` +
-          `Nome Cardápio: ${order.menuName}\n` +
-          `Nome do cliente: ${order.clientName}\n` +
-          `Número do telefone: ${order.clientPhoneNumber}\n` +
-          `Forma de pagamento: ${order.paymentMethod}\n` +
-          `Tipo de entrega: ${order.deliveryType}\n` +
-          `Tempo estimado de entrega: ${order.deliveryTime}\n` +
-          `Endereço para entrega: ${order.deliveryAddress}\n` +
-          `${resumoPedidoTexto}\n` +
-          `Taxa de entrega: R$ 3,00\n` +
-          `TOTAL: ${priceToBrazilCurrency(totalPrice)}` // here the delivery tax has to be added
-      )}`;
 
       window.location.href = whatsappURL;
     } catch (error) {
       toast.error(`Houve um problema em registrar seu Pedido, tente novamente`);
     } finally {
+      handleClearCartItems();
       setGeneratingOrder(false);
     }
   };
@@ -191,78 +155,12 @@ export default function ShoppingCartPortal({
               </button>
             </div>
 
-            <div className={styles.shoppingCartContainerProducts}>
-              {cartItens.length > 0 ? (
-                cartItens.map((cartItem) => (
-                  <div
-                    key={cartItem.id}
-                    className={styles.shoppingCartContainerProduct}
-                  >
-                    <div className={styles.shoppingCartContainerProductImage}>
-                      <Image
-                        src={cartItem?.productImage}
-                        alt={`Foto do produto ${cartItem.productName}`}
-                        fill
-                      />
-                    </div>
-                    <div
-                      className={styles.shoppingCartContainerProductInformation}
-                    >
-                      <div
-                        className={
-                          styles.shoppingCartContainerProductTitleAndEditIcon
-                        }
-                      >
-                        <p className={styles.shoppingCartBoldTexts}>
-                          {cartItem.productName}
-                        </p>
-                        <FaRegEdit size={16} />
-                      </div>
-                      <div className={styles.shoppingCartContainerComplements}>
-                        {cartItem.allSelectedComplements.flatMap(
-                          (selectedComplement) =>
-                            selectedComplement.items.map((item) => (
-                              <p
-                                key={item.id}
-                                className={styles.shoppingCartComplementsTexts}
-                              >
-                                {`${item.amount} ${item.name}`}
-                              </p>
-                            ))
-                        )}
-                      </div>
+            <ShoppingCartProducts
+              cartItens={cartItens}
+              removeCartProductAmount={removeCartProductAmount}
+              addCartProductAmount={addCartProductAmount}
+            />
 
-                      <div className={styles.shoppingContainerPriceAndQuantity}>
-                        <p className={styles.shoppingCartBoldTexts}>
-                          {`${priceToBrazilCurrency(cartItem.totalValue)}`}
-                        </p>
-                        <div className={styles.addMoreToCart}>
-                          <button
-                            type="button"
-                            onClick={() => {
-                              removeCartProductAmount(cartItem);
-                            }}
-                            aria-label="Remover item do carrinho"
-                          >
-                            <FaMinus size={14} color="#EF4444" />
-                          </button>
-                          <p>{cartItem.amount}</p>
-                          <button
-                            type="button"
-                            onClick={() => addCartProductAmount(cartItem)}
-                            aria-label="Adicionar item ao carrinho"
-                          >
-                            <FaPlus size={14} />
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                ))
-              ) : (
-                <p>Não existem produtos no carrinho</p>
-              )}
-            </div>
             <button
               type="button"
               className={styles.shoppingCartAddMoreItensButton}
