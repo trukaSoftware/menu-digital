5e580c5659795dedcde24db3ae54bb0d18a7061f
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-10-23 11:18:22 -0300

    refactor: add logic to rendering and download a table number of qrcodes

--- src/components/QrCode/index.tsx
+++ src/components/QrCode/index.tsx
@@ -1,45 +1,70 @@
 import { useState } from 'react';
 import QRCode from 'react-qr-code';
 
+import JSZip from 'jszip';
+
 import { useUser } from '@clerk/nextjs';
 
 import styles from './styles.module.css';
 
-export default function QrCode() {
+interface QrCodeProps {
+  setShowDialog: (value: boolean) => void;
+  qrCodeProductsUrl: string[];
+}
+
+export default function QrCode({
+  qrCodeProductsUrl,
+  setShowDialog,
+}: QrCodeProps) {
   const { user } = useUser();
   const [qrCodeButtonTitle, setQrCodeButtonTitle] = useState(`Salvar QRCode`);
 
-  const fullURL = window.location.href;
-  const urlObj = new URL(fullURL);
-  const pathname = urlObj.origin;
-
-  const qrCodeProductsUrl = `${pathname}/produtos/${user?.publicMetadata.slug}/${user?.id}`;
-
   const handleSaveQrCode = async () => {
-    const canvas = document.getElementById(
-      `productsQrCode`
-    ) as HTMLCanvasElement;
-    const image = new Image();
-    image.src = `data:image/svg+xml;base64,${btoa(
-      new XMLSerializer().serializeToString(canvas)
-    )}`;
+    const zip = new JSZip();
+
+    qrCodeProductsUrl.forEach((qrcode, index) => {
+      const canvas = document.getElementById(
+        `qrcode-table-${index + 1}`
+      ) as HTMLCanvasElement;
+
+      const svgData = new XMLSerializer().serializeToString(canvas);
+
+      zip.file(
+        `QRCode-${user?.publicMetadata.slug}-Mesa ${index + 1}.svg`,
+        svgData
+      );
+    });
+
     try {
-      const downloadLink = document.createElement(`a`);
-      downloadLink.href = image.src;
-      downloadLink.download = `QRCode-${user?.publicMetadata.slug}`;
-      document.body.appendChild(downloadLink);
-      downloadLink.click();
-      document.body.removeChild(downloadLink);
+      zip.generateAsync({ type: `blob` }).then((content: any) => {
+        const downloadLink = document.createElement(`a`);
+        downloadLink.href = URL.createObjectURL(content);
+        downloadLink.download = `QRCode-${user?.publicMetadata.slug}.zip`;
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+      });
+
       setQrCodeButtonTitle(`QRCode salvo!`);
     } catch (error) {
       setQrCodeButtonTitle(`Erro ao salvar, tente novamente.`);
+    } finally {
+      setShowDialog(false);
     }
   };
 
   return (
     <>
       <div className={styles.qrCodeWrapper}>
-        <QRCode value={qrCodeProductsUrl} id="productsQrCode" />
+        <div className={styles.qrCodeContainer}>
+          {qrCodeProductsUrl?.map((qrcode, index) => (
+            <QRCode
+              key={qrcode}
+              value={qrcode}
+              id={`qrcode-table-${index + 1}`}
+            />
+          ))}
+        </div>
         <button
           type="button"
           className={styles.qrCodeButton}
