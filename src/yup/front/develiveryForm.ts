b0f1e7f65e7d2a851e41ee86c609ac4c5942d937
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-10-30 12:04:33 -0300

    feat: create deliverySchema

---
+++ src/yup/front/develiveryForm.ts
@@ -0,0 +1,28 @@
+import { object, string, InferType } from 'yup';
+
+export const deliverySchema = object({
+  clientName: string().required(
+    `É preciso preencher este campo com o nome do produto.`
+  ),
+  clientPhoneNumber: string()
+    .min(10, `O Telefone é obrigatório.`)
+    .max(11, `O número de telefone deve ser válido.`)
+    .required(),
+  deliveryType: string().required(
+    `É preciso selecionar ao menos uma categoria para o produto.`
+  ),
+  paymentMethod: string().required(
+    `É preciso preencher este campo com o preço do produto.`
+  ),
+  deliveryTime: string().required(
+    `É preciso selecionar ao menos uma categoria para o produto.`
+  ),
+  deliveryAddress: string().required(
+    `É preciso selecionar ao menos uma categoria para o produto.`
+  ),
+});
+
+export type DeliveryData = InferType<typeof deliverySchema>;
+
+export const validateDeliveryData = (deliveryData: DeliveryData) =>
+  deliverySchema.validate(deliveryData);
