ea31ba6f6d84dc49a40e7d6e6dace96157db67fb
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-11-01 15:59:58 -0300

    feat: create component PaymentInfo

---
+++ src/components/Dialogs/ShoppingCartDialog/PaymentInfo/index.tsx
@@ -0,0 +1,57 @@
+import { UseFormRegisterReturn } from 'react-hook-form';
+import {
+  BsFillCreditCard2BackFill,
+  BsFillCreditCardFill,
+} from 'react-icons/bs';
+import { FaMoneyBill } from 'react-icons/fa';
+import { FaPix } from 'react-icons/fa6';
+
+import DefaultRadioInput from '@/components/DefaultRadioInput';
+
+import styles from './styles.module.css';
+
+export interface PaymentInfoProps {
+  register: UseFormRegisterReturn;
+}
+
+export default function PaymentInfo({ register }: PaymentInfoProps) {
+  return (
+    <div className={styles.paymentInfoWrapper}>
+      <h2>Escolha a forma de pagamento</h2>
+      <div className={styles.paymentInfoRadios}>
+        <DefaultRadioInput
+          id="pix"
+          value="pix"
+          inputText="Pix"
+          register={register}
+          labelClassName={styles.paymentInfoRadio}
+          Icon={<FaPix size={22} />}
+        />
+        <DefaultRadioInput
+          id="credit"
+          inputText="Cartão de credito"
+          value="Cartão de crédito"
+          labelClassName={styles.paymentInfoRadio}
+          register={register}
+          Icon={<BsFillCreditCard2BackFill size={22} />}
+        />
+        <DefaultRadioInput
+          id="money"
+          value="Dinheiro"
+          inputText="Dinheiro"
+          register={register}
+          labelClassName={styles.paymentInfoRadio}
+          Icon={<FaMoneyBill size={22} />}
+        />
+        <DefaultRadioInput
+          id="debt"
+          inputText="Cartão de débito"
+          value="Cartão de débito"
+          labelClassName={styles.paymentInfoRadio}
+          register={register}
+          Icon={<BsFillCreditCardFill size={22} />}
+        />
+      </div>
+    </div>
+  );
+}
