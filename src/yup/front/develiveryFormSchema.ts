7c44e8cc27428f6db2b3137ebb93bdeaf504d08a
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-11-01 16:00:57 -0300

    refactor: adjust schemas to matche front end  values

--- src/yup/front/develiveryFormSchema.ts
+++ src/yup/front/develiveryFormSchema.ts
@@ -9,16 +9,13 @@ export const deliverySchema = object({
     .max(11, `O número de telefone deve ser válido.`)
     .required(),
   deliveryType: string()
-    .oneOf([`pickup`, `delivery`])
+    .oneOf([`Retirada`, `Entrega`])
     .required(`É preciso selecionar ao menos uma categoria para o produto.`),
-  paymentMethod: string().required(
-    `É preciso preencher este campo com o preço do produto.`
-  ),
-  deliveryTime: string().required(
-    `É preciso selecionar ao menos uma categoria para o produto.`
-  ),
+  paymentMethod: string()
+    .oneOf([`pix`, `Cartão de crédito`, `Dinheiro`, `Cartão de débito`])
+    .required(`É preciso preencher este campo com o preço do produto.`),
   deliveryAddress: string().required(
-    `É preciso selecionar ao menos uma categoria para o produto.`
+    `É preciso preencher o campo de endereço corretamente.`
   ),
 });
 
