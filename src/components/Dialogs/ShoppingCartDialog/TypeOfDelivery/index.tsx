9cc07c666457eb8d9ea216408037bd4ff5abc4d3
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-11-01 09:43:00 -0300

    feat: create component TypeOfDelivery

--- src/components/Dialogs/ShoppingCartDialog/TypeOfDelivery/index.tsx
+++ src/components/Dialogs/ShoppingCartDialog/TypeOfDelivery/index.tsx
@@ -1,18 +1,30 @@
+import { UseFormRegisterReturn } from 'react-hook-form';
+
+import DefaultRadioInput from '@/components/DefaultRadioInput';
+
 import styles from './styles.module.css';
 
-export default function TypeOfDelivery() {
+export interface TypeOfDeliveryProps {
+  register: UseFormRegisterReturn;
+}
+
+export default function TypeOfDelivery({ register }: TypeOfDeliveryProps) {
   return (
     <div className={styles.typeOfDeliveryWrapper}>
       <h2>Escolha o tipo de entrega</h2>
       <div className={styles.typeOfDeliveryCheckboxesWrapper}>
-        <label htmlFor="pickup">
-          <input id="pickup" type="radio" name="typeOfDelivery" />
-          Retirada
-        </label>
-        <label htmlFor="delivery">
-          <input id="delivery" type="radio" name="typeOfDelivery" />
-          Delivery
-        </label>
+        <DefaultRadioInput
+          id="delivery"
+          value="delivery"
+          inputText="Entrega"
+          register={register}
+        />
+        <DefaultRadioInput
+          id="pickup"
+          value="pickup"
+          inputText="Retirada"
+          register={register}
+        />
       </div>
     </div>
   );
