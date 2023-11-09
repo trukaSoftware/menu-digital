43de8e565e03c7f6935d0e1f7c320a35b4bb6c02
Eberton <ebertonsouza@unigranrio.br>
2023-11-09 02:55:00 -0300

    refactor: add params to get complements

--- src/utils/api/getComplements.ts
+++ src/utils/api/getComplements.ts
@@ -2,9 +2,9 @@ import { GetComplementReturn } from '@/types/complement';
 
 import api from '@/utils/api';
 
-export async function getComplements() {
+export async function getComplements(companyId: string) {
   const data = await api.get<GetComplementReturn>(
-    `/complements/getComplements`
+    `/complements/getComplements?companyId=${companyId}`
   );
 
   return { complements: data?.data?.complements };
