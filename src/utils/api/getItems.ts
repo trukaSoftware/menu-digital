d0a43514f4fc75a13ceb184c7150308714dfa17e
Eberton <ebertonsouza@unigranrio.br>
2023-11-09 21:29:27 -0300

    refactor: use companyId to get items

--- src/utils/api/getItems.ts
+++ src/utils/api/getItems.ts
@@ -2,8 +2,10 @@ import { GetItemReturn } from '@/types/item';
 
 import api from '@/utils/api';
 
-export async function getItems() {
-  const data = await api.get<GetItemReturn>(`/items/getItems`);
+export async function getItems(companyId: string) {
+  const data = await api.get<GetItemReturn>(
+    `/items/getItems?companyId=${companyId}`
+  );
 
   return { items: data?.data?.items };
 }
