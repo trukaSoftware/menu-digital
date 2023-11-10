6cf3874280e9cc0c40c7776d76782f31ec9b8e89
Eberton <ebertonsouza@unigranrio.br>
2023-11-09 21:23:29 -0300

    feat: use redux to delete complements

--- src/components/Dialogs/DeleteComplementDialog/index.tsx
+++ src/components/Dialogs/DeleteComplementDialog/index.tsx
@@ -2,12 +2,20 @@
 
 import { useState } from 'react';
 import { FaRegTrashAlt } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
+
+import { useParams } from 'next/navigation';
 
 import axios from 'axios';
 
 import ButtonSubmit from '@/components/ButtonSubmit';
 
+import { getComplements } from '@/utils/api/getComplements';
+import { getItems } from '@/utils/api/getItems';
+
+import { setComplements } from '@/redux/features/complements-slice';
+import { setItems } from '@/redux/features/items-slice';
 import {
   Root,
   Trigger,
@@ -29,6 +37,8 @@ export default function DeleteComplementDialog({
   const [showDialog, setShowDialog] = useState(false);
   const [deleteError, setDeleteError] = useState(``);
   const [isDeleting, setIsDeleting] = useState(false);
+  const params = useParams();
+  const dispatch = useDispatch();
 
   const deletedFailed = () => {
     setIsDeleting(false);
@@ -45,6 +55,10 @@ export default function DeleteComplementDialog({
 
       if (deletedComplement.data?.deleted) {
         setShowDialog(false);
+        const allComplements = await getComplements(params.companyId);
+        const allItems = await getItems(params.companyId);
+        dispatch(setComplements(allComplements.complements));
+        dispatch(setItems(allItems.items));
         toast.success(`Adicional deletado com sucesso!`);
         return;
       }
